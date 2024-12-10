c8fcd1a488f7bf5d47ac987a6cb6db9f78b14a71
Jacob M <114640505+JMestre32@users.noreply.github.com>
2024-12-10 04:16:07 -0800

    Start working on js

---
+++ library.js
@@ -0,0 +1,23 @@
+const myLibrary = [];
+
+function Book(author, title, pages, read){
+
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(newBook){
+    const exists = myLibrary.find(book => book.title === newBook.title);
+    if(!exists){
+        myLibrary.push(newBook);
+    } else {
+        console.log(newBook.title + " exists in your library already");
+    }
+    
+}
+
+const book1 = new Book("Jeff Kinney", "Diary of a Wimpy Kid", 150, false);
+
+addBookToLibrary(book1);
