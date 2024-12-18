661baf59714912f88800b1b130029106c011e67b
Jacob M <114640505+JMestre32@users.noreply.github.com>
2024-12-18 12:14:29 -0800

    Added remove button

--- library.js
+++ library.js
@@ -47,15 +47,26 @@ const bookForm = document.querySelector('.book-form')
 submitButton.addEventListener('click', (book) =>{
     const titleExists = myLibrary.some(book => book.title.toLowerCase() === bookForm.title.value.toLowerCase());
     if (titleExists) {
-        const message = document.createElement("p");
-        message.id = "errorMessage";
-        message.textContent = bookForm.title.value + " exists in your library already";
-        errorMessageSpace.appendChild(message);
-        messageExists = true;
+        if(messageExists){
+            const target = document.getElementById("errorMessage");
+            target.remove();
+            messageExists = false;
+            modal.close();
+            return
+        }
+        else{
+            const message = document.createElement("p");
+            message.id = "errorMessage";
+            message.textContent = bookForm.title.value + " exists in your library already";
+            errorMessageSpace.appendChild(message);
+            messageExists = true;
+            return
+
+        }
+    }
+    else if(bookForm.author.value === ""|| bookForm.title.value === "" || bookForm.pages.value ===""){
         return
     }
-
-
     const newBook = new Book(bookForm.author.value, bookForm.title.value, bookForm.pages.value, bookForm.read.checked);
     addBookToLibrary(newBook);
 
@@ -65,6 +76,8 @@ submitButton.addEventListener('click', (book) =>{
 
     const addedBook = myLibrary[myLibrary.length - 1]
 
+    
+    bookInfo.innerHTML = `<div class = delete> </div>`;
     bookInfo.innerHTML = `<strong>Author: </strong>`;
     bookInfo.appendChild(document.createTextNode(addedBook.author));
     bookInfo.innerHTML += `<br><strong>Title: </strong>`;
@@ -75,6 +88,9 @@ submitButton.addEventListener('click', (book) =>{
     readButton.textContent = addedBook.read ? "Read" : "Not Read";
     readButton.style.backgroundColor = addedBook.read ? "green" : "red";
     readButton.classList.add("read-button");
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Remove";
+    deleteButton.classList.add("delete-button");
 
     readButton.addEventListener('click', function() {
         addedBook.read = !addedBook.read;
@@ -82,8 +98,20 @@ submitButton.addEventListener('click', (book) =>{
         readButton.style.backgroundColor = addedBook.read ? "green" : "red";
     });
 
+    deleteButton.addEventListener('click', function(){
+        const index = myLibrary.findIndex(book => book.title === addedBook.title);
+        console.log(index);
+
+        if (index !== -1) {
+            myLibrary.splice(index, 1);
+        }
+        this.closest('.book').remove();
+    })
+
     bookInfo.appendChild(document.createElement("br"));
     bookInfo.appendChild(readButton);
+    bookInfo.appendChild(document.createElement("br"));
+    bookInfo.appendChild(deleteButton);
 
     box.classList.add("book");
     books.appendChild(box);
